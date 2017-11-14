187ab6849f53bd43816073fad053fb029f71f145
caco <crns13@gmail.com>
2017-11-14 19:18:08 -0200

    Make second form class, inheareted from the first one, to handle posting duplicate list items; skip a view test by now

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -29,8 +29,7 @@ class ItemModelsTest(TestCase):
         Item.objects.create(list=list_, text='bla')
         with self.assertRaises(ValidationError):
             item = Item(list=list_, text='bla')
-            # item.full_clean()
-            item.save()
+            item.full_clean()
 
     def test_CAN_save_same_item_to_different_lists(self):
         list1 = List.objects.create()
